f86d7e1660a3a30c3e4e29fadc83b82c19d2a017
ire giro <einstyle01@gmail.com>
2017-05-16 22:56:15 +0200

    Hey folks!

---
+++ Assets/my_scripts/PubNubReceiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PubNubMessaging.Core;
+
+public class PubNubReceiver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Pubnub pubnub = new Pubnub("demo", "demo");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
